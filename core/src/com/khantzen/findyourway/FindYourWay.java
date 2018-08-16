cf021bbd8d8d2599e4264d4a99c32cc85d65fc06
khantzen <kevin.hantzen@gmail.com>
2018-08-16 20:29:26 +0200

    Indent with spaces

--- core/src/com/khantzen/findyourway/FindYourWay.java
+++ core/src/com/khantzen/findyourway/FindYourWay.java
@@ -1,7 +1,4 @@
 package com.khantzen.findyourway;
-// @TODO: Kevin, consider registering domain khantzen.com if you plan to use com.khanten package :).
-// @TODO: Consider indenting with spaces :TROLL:
-// @TODO: BUT. WHERE. ARE. THE. TESTS.
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
@@ -17,33 +14,33 @@ public class FindYourWay extends ApplicationAdapter {
     private ShapeRenderer shapeRenderer;
 
     @Override
-	public void create () {
-		this.batch = new SpriteBatch();
-		this.shapeRenderer = new ShapeRenderer();
-		this.gameBoard = new GameBoard();
-		this.gameBoard.init();
-	}
+    public void create() {
+        this.batch = new SpriteBatch();
+        this.shapeRenderer = new ShapeRenderer();
+        this.gameBoard = new GameBoard();
+        this.gameBoard.init();
+    }
 
-	@Override
-	public void render () {
+    @Override
+    public void render() {
         this.gameBoard.calculate();
 
         Gdx.gl.glClearColor(1, 1, 1, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         Gdx.gl.glEnable(GL20.GL_BLEND);
         Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
         Gdx.gl.glDisable(GL20.GL_BLEND);
 
-		batch.begin();
-		this.gameBoard.renderText(batch);
+        batch.begin();
+        this.gameBoard.renderText(batch);
         batch.end();
 
         this.gameBoard.renderObject(this.shapeRenderer);
     }
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
+
+    @Override
+    public void dispose() {
+        batch.dispose();
     }
 }
